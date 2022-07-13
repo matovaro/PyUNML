f9d348752c8c573d59cbbf01bfe70637d86b83bf
Mike <matovaro@unal.edu.co>
2022-07-12 22:10:30 -0500

    FileGenerator v2

--- Generators/DiagramFileGenerator.py
+++ Generators/DiagramFileGenerator.py
@@ -21,3 +21,56 @@ class DiagramFileGenerator:
         EntFile.close()
         
         return nameFile
+
+    def ClassFile(self, arrayClasses):
+        nameFile = self.FileName + '-Classes.txt'
+
+        classes = arrayClasses['Clases']
+        relations = arrayClasses['Relaciones']
+
+        ClassFile = open(nameFile,'a')
+        for index in classes.keys():
+            ClassFile.write('class ' + index + ' {' +'\n')
+
+            for attr in classes[index]['Atributos']:
+                ClassFile.write('   ' + attr +'\n')
+
+            for met in classes[index]['Metodos']:
+                ClassFile.write('   ' + met +'() \n')
+
+            ClassFile.write('}' +'\n')
+        
+        for relation in relations:
+            if relation[2] == 'ASOC':
+                ClassFile.write(relation[0] + ' -- ' + relation[1] +'\n')
+
+            if relation[2] == 'AGR':
+                ClassFile.write(relation[0] + ' o-- ' + relation[1] +'\n')
+
+            if relation[2] == 'COMP':
+                ClassFile.write(relation[0] + ' *-- ' + relation[1] +'\n')
+
+            if relation[2] == 'HER':
+                ClassFile.write(relation[0] + ' <|-- ' + relation[1] +'\n')
+            
+            if relation[2] == 'DEP':
+                ClassFile.write(relation[0] + ' ..> ' + relation[1] +'\n')
+            
+
+        ClassFile.close()
+
+        return nameFile
+
+    def CaseUseFile(self, CaseUseArray):
+        nameFile = self.FileName + '-CaseUse.txt'
+
+        CaseFile = open(nameFile,'a')
+
+        CaseFile.write('skinparam actorStyle awesome' +'\n')
+        for actor in CaseUseArray.keys():
+            for case in CaseUseArray[actor]:
+                CaseFile.write(':' + actor + ': --> (' + case + ')' +'\n')
+
+        CaseFile.close()
+
+        return nameFile
